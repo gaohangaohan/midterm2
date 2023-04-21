78d3215188e9f2da7343eab34b5ba20452d5ee7c
gaohangaohan <28073856+gaohangaohan@users.noreply.github.com>
2023-04-21 15:01:17 -0400

    Add files via upload

--- tsne.R
+++ tsne.R
@@ -1,12 +1,15 @@
 library(Rtsne)
+library(factoextra)
 
 data <- as.data.frame(read.table("coded.txt", header = T))
 data$continent = as.factor(data$continent)
 colors = rainbow(length(unique(data$continent)))
 names(colors) = unique(data$continent)
-tsne <- Rtsne(data[,-1:-3], dims = 1, perplexity=30, verbose=TRUE, max_iter = 5000)
+tsne <- Rtsne(data[,-1:-3], dims = 2, perplexity=30, verbose=TRUE, max_iter = 5000)
 par(mar = c(5,4,4,15),xpd=T)
 plot(tsne$Y, t='n', main="tsne")
 points(tsne$Y, col=colors[data$continent], pch = 20)
 legend("topright", inset = c(- 1, 0), legend = names(colors), col = colors[names(colors)], pch =20)
-
+transform = as.data.frame(tsne$Y[,c(1,2)])
+kmeans_h = kmeans(transform, centers = 6)
+fviz_cluster(kmeans_h, data = transform, geom = "point")
